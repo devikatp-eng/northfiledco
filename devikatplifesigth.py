decb661f19753c91eba68f3ad5094406f51d6930
devikatp-eng <devika.tp@stat.christuniversity.in>
2026-09-18 16:04:03 +0530

    Update devikatplifesigth.py

--- devikatplifesigth.py
+++ devikatplifesigth.py
@@ -7,14 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1f9NVB7CqT5ho2zKgIwBYSSbVn2FFzeAo
 """
 
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import warnings
-warnings.filterwarnings('ignore')
-
 pd.set_option('display.float_format', lambda x: f'{x:,.2f}')
 print('Libraries loaded.')
 
